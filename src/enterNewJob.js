34ab5ae9dc78a674b0a4d222e9e35b9fbf59f5c1
zigzbox <glen.hecht@gmail.com>
2018-12-30 20:48:59 -0700

    first commit

---
+++ src/enterNewJob.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+
+
+class enterNewJob extends Component {
+render(){
+    return(
+        <div>
+            <header><h1>Look at all the jobs!</h1></header>
+            <div>
+                <button>enter new job here</button>
+            </div>
+
+        </div>
+    )
+}
+}
+
+export default enterNewJob
