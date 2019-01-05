8bf4fa570712ce051243d1a555bc897d9072cdf6
zigzbox <glen.hecht@gmail.com>
2019-01-05 16:19:11 -0700

    basic mach up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import enterNewJob from './enterNewJob';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div>
+        <header>
+
         </header>
+        <Switch>
+            <Route exact path='/' component={enterNewJob} />
+
+
+        </Switch>
+
+
       </div>
     );
   }
--- src/enterNewJob.js
+++ src/enterNewJob.js
@@ -4,17 +4,36 @@ import React, { Component } from 'react'
 
 
 class enterNewJob extends Component {
-render(){
-    return(
-        <div>
-            <header><h1>Look at all the jobs!</h1></header>
+    render() {
+        return (
             <div>
-                <button>enter new job here</button>
-            </div>
+                <header><h1>Glen's Job App Tracker</h1></header>
+                <div>
+                    <label htmlFor='companyName'>Company Name:</label>
+                    <input id='companyName' type='text' />
+                    <label htmlFor='dateContacted'>Date Contacted:</label>
+                    <input id='dateContacted' type='date' />
+                    <label htmlFor='nameOfContact'>Name of Contact:</label>
+                    <input id='nameOfContact' type='text' />
+                    <label htmlFor='interveiw'>Interview:</label>
+                    <span>Yes</span>
+                    <input id='interveiw' type='checkbox' />
+                    <span>No</span>
+                    <input id='interveiw' type='checkbox' />
+                    <div>
+                        <button>enter new job here</button>
+                    </div>
+                </div>
 
-        </div>
-    )
-}
+                <div>
+                    <h3>Recent Job Apps</h3>
+                    {/* this is where ill pull from the back end from the database of jobs and display 10 most recent */}
+                </div>
+
+
+            </div>
+        )
+    }
 }
 
 export default enterNewJob
